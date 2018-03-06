f8dc1477ca2f012b432522d44c94456404be2e02
phw <phw@civadis.be>
2018-03-06 15:44:13 +0100

    modif version

--- src/test/java/org/activiti/HireProcessTest.java
+++ src/test/java/org/activiti/HireProcessTest.java
@@ -11,8 +11,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.subethamail.wiser.Wiser;
@@ -20,9 +20,8 @@ import org.subethamail.wiser.Wiser;
 import java.util.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {MyApp.class})
+@SpringBootTest
 @WebAppConfiguration
-@IntegrationTest
 public class HireProcessTest {
 
     @Autowired
